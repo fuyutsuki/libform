d4d45f5dcc267d5359fe78a072bdf66d1d234a3d
yuko fuyutsuki <yukofuyutsuki@gmail.com>
2018-09-05 15:02:11 +0900

    0.4.0 * Removed responseInt2Str()

--- src/tokyo/pmmp/libform/EventListener.php
+++ src/tokyo/pmmp/libform/EventListener.php
@@ -5,9 +5,6 @@ use pocketmine\event\Listener;
 use pocketmine\event\player\PlayerQuitEvent;
 use pocketmine\event\server\DataPacketReceiveEvent;
 use pocketmine\network\mcpe\protocol\ModalFormResponsePacket;
-use tokyo\pmmp\libform\element\Dropdown;
-use tokyo\pmmp\libform\element\StepSlider;
-use tokyo\pmmp\libform\form\CustomForm;
 
 class EventListener implements Listener {
 
@@ -23,9 +20,6 @@ class EventListener implements Listener {
         }
         $callable = $form->getCallable();
         if ($callable !== null) {
-          if ($form instanceof CustomForm) {
-            $response = $this->responseInt2Str($form, $response);
-          }
           $callable($player, $response);
         }
         FormApi::removeForm($formId);
@@ -42,25 +36,5 @@ class EventListener implements Listener {
         break;
       }
     }
-  }
-
-  private function responseInt2Str(CustomForm $form, array $response = null): ?array {
-    if ($response !== null) {
-      $elements = $form->getElements();
-      foreach ($elements as $key => $element) {
-        switch (true) {
-          case $element instanceof Dropdown:
-            $str = $element->getOption($response[$key]);
-            $response[$key] = $str;
-            break;
-
-          case $element instanceof StepSlider:
-            $str = $element->getStep($response[$key]);
-            $response[$key] = $str;
-            break;
-        }
-      }
-    }
-    return $response;
   }
 }
